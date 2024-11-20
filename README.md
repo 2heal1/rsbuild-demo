273b5fd5bf349dc17cd8de52a02873fd0154a9ed
2heal1 <TwoHeal@163.com>
2024-11-20 17:38:06 +0800

    chore: add demo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Setup
+## How to reproduce
 
 Install the dependencies:
 
@@ -6,10 +6,19 @@ Install the dependencies:
 pnpm install
 ```
 
-## Get Started
-
-Start the dev server:
+build
 
 ```bash
-pnpm dev
+pnpm build
+```
+
+And the terminal will print the error message:
+```
+error   [@rsbuild/plugin-check-syntax] Find some syntax that does not match "ecmaVersion <= 2017":
+
+  ERROR 1
+  source:  /dist/static/js/cc.ee2631e0.js:1320:16
+  reason:  'import' and 'export' may only appear at the top level (1320:16)
+  code:    (!remoteEntryExports) {
+                import(/* webpackIgnore: true */ entry).
 ```
--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,22 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import {ModuleFederationPlugin} from '@module-federation/enhanced/rspack'
+import { pluginCheckSyntax } from "@rsbuild/plugin-check-syntax";
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [pluginReact(),pluginCheckSyntax()],
+  tools:{
+    rspack(config){
+      config.optimization!.minimize= false;
+      config.plugins!.push(new ModuleFederationPlugin({
+        name:'cc',
+        exposes:{
+          '.':'./src/index.tsx'
+        }
+      }))
+    }
+  },
+  source:{
+    include:[/@module-federation\/runtime/]
+  },
 });
